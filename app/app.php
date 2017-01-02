e300014c4698ba38da4fa0c23bb6e643da2ec4e0
zach <zhedgepeth>
2017-01-02 09:40:31 -0500

    set timezone to america/new_york solving all errors

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
 
+    date_default_timezone_set('America/New_York');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
     require_once __DIR__."/../src/Client.php";
